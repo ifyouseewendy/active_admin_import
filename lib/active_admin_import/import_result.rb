a5eb60874c28ca89e8f28c77f4d6cf83d99640e1
wendi <ifyouseewendy@gmail.com>
2015-08-13 12:07:01 +0800

    Yield message when import failed on record errors

--- lib/active_admin_import/import_result.rb
+++ lib/active_admin_import/import_result.rb
@@ -11,21 +11,25 @@ module ActiveAdminImport
        @failed += result.failed_instances
        @total+=qty
     end
-        
+
     def imported_qty
       total - failed.count
-    end 
-    
+    end
+
     def has_imported?
       imported_qty > 0
     end
-    
+
     def has_failed?
       @failed.any?
     end
 
     def empty?
       total == 0
+    end
+
+    def failed_message
+      failed.map.with_index{|record, i| "##{i+1} - #{record.errors.full_messages.join(', ')}" }.join("\n")
     end
 
   end
