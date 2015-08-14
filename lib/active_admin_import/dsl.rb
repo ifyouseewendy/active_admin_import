485a6ed496194628b44afd78ed132a426dd31e72
wendi <ifyouseewendy@gmail.com>
2015-08-14 12:44:08 +0800

    Limit on failed message size*
    Concern CookieOverflowError in PR #59, https://github.com/activeadmin-plugins/active_admin_import/pull/59#ref-pullrequest-100829708

--- lib/active_admin_import/dsl.rb
+++ lib/active_admin_import/dsl.rb
@@ -33,7 +33,7 @@ module ActiveAdminImport
         flash[:warning] = I18n.t('active_admin_import.file_empty_error')
       else
         if result.has_failed?
-          flash[:error] = I18n.t('active_admin_import.failed', count: result.failed.count, model: model_name, plural_model: plural_model_name, message: result.failed_message)
+          flash[:error] = I18n.t('active_admin_import.failed', count: result.failed.count, model: model_name, plural_model: plural_model_name, message: result.failed_message(limit: 5))
           return if options[:batch_transaction]
         end
         if result.has_imported?
